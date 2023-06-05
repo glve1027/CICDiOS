ad799edde4aa0a446777a9ffc0aed37813df0e68
GhoooohG <115147682+GhoooohG@users.noreply.github.com>
2023-06-05 21:37:59 +0800

    Initial Commit

---
+++ RetirementCalculator/RetirementCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetirementCalculatorApp.swift
+//  RetirementCalculator
+//
+//  Created by GH on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct RetirementCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
